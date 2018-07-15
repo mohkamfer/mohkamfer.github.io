339bd88a101f1d70301aa141f1011a2e1b814b17
Muhammad Kamal Fergany <mohammedkamalfergany@yahoo.com>
2018-07-15 09:08:17 +0200

    Main Cover Overhaul
    Establish a newer design, remove old navbar

--- src/main.js
+++ src/main.js
@@ -2,45 +2,7 @@ var nav = document.getElementById('nav');
 var cover = document.getElementById('cover');
 var toTop = document.getElementById('to-top');
 
-function main() {
-    initNav();
-    initToTop();
-    document.getElementById('arrow').addEventListener('click', arrowClick);
-}
-document.addEventListener('DOMContentLoaded', main);
-
-function initNav() {
-    var javaSection = document.getElementById('java');
-    var androidSection = document.getElementById('android');
-    var webSection = document.getElementById('web');
-    
-    var javaButton = document.getElementById('java-button');
-    var androidButton = document.getElementById('android-button');
-    var webButton = document.getElementById('web-button');
-    
-    javaButton.addEventListener('click', function() {
-        javaSection.scrollIntoView({ behavior: 'smooth' });
-    });
-
-    androidButton.addEventListener('click', function() {
-        androidSection.scrollIntoView({ behavior: 'smooth' });
-    });
-    
-    webButton.addEventListener('click', function() {
-        webSection.scrollIntoView({ behavior: 'smooth' });
-    });
-    
-    document.addEventListener('scroll', function() {
-        if (document.body.scrollTop > cover.clientHeight) {
-            nav.classList.add('sticky');
-        } else {
-            nav.classList.remove('sticky');
-        }
-    });
-}
-
 function arrowClick() {
-    var start = document.body.scrollTop;
     var end = cover.clientHeight + 1;
     window.scroll({
         top: end, 
@@ -58,3 +20,9 @@ function initToTop() {
         }
     });
 }
+
+function main() {
+    initToTop();
+    document.getElementById('arrow').addEventListener('click', arrowClick);
+}
+document.addEventListener('DOMContentLoaded', main);
