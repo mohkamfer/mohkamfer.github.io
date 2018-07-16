66f12d964d25b67bd8ed16939f13848df390da5d
Muhammad Kamal Fergany <mohammedkamalfergany@yahoo.com>
2018-07-16 10:05:50 +0200

    Made Nav Functional
    Gave life to the navbar buttons when clicked and when scrolling

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 var nav = document.getElementById('nav');
 var cover = document.getElementById('cover');
 var toTop = document.getElementById('to-top');
+var doButton = document.getElementById('do');
+var didButton = document.getElementById('did');
+var contactButton = document.getElementById('contact');
+var doAnchor = document.getElementById('do-a');
+var didAnchor = document.getElementById('java-a');
+var contactAnchor = document.getElementById('contact-a');
 
 function arrowClick() {
     var end = cover.clientHeight + 1;
@@ -12,13 +18,40 @@ function arrowClick() {
 }
 
 function initToTop() {
-    document.addEventListener('scroll', function() {
-        if (document.body.scrollTop > cover.clientHeight) {
+    document.addEventListener('scroll', function(e) {
+        var currentScroll = document.body.scrollTop;
+        if (currentScroll > cover.clientHeight) {
             toTop.classList.remove('hidden');
         } else {
             toTop.classList.add('hidden');
         }
+        
+        if ((doAnchor.offsetTop - 300 < currentScroll) && (didAnchor.offsetTop - 200 > currentScroll)) {
+            doButton.classList.add('active');
+        } else {
+            doButton.classList.remove('active');
+        }
+        
+        if ((didAnchor.offsetTop - 200 < currentScroll) && (contactAnchor.offsetTop - 200 > currentScroll)) {
+            didButton.classList.add('active');
+        } else {
+            didButton.classList.remove('active');
+        }
+        
+        if (contactAnchor.offsetTop - 200 < currentScroll) {
+            contactButton.classList.add('active');
+        } else {
+            contactButton.classList.remove('active');
+        }
     });
+    
+    doButton.addEventListener('click', function() {
+        document.getElementById('do-a').scrollIntoView({behavior: 'smooth', block: 'start'});
+    }, false);
+    
+    didButton.addEventListener('click', function() {
+        document.getElementById('java-a').scrollIntoView({behavior: 'smooth', block: 'start'});
+    }, false);
 }
 
 function main() {
